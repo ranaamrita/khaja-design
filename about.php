9aa5670294c5bb83d4dde62d32680a2ac986b746
chysharmila <sharmilchudh547@gmail.com>
2019-01-26 17:38:10 +0545

    about us page design

---
+++ about.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/css/index1.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+  <script src="assets/js/jquery.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="assets/css/about.css">
+</head>
+<body>
+<div class="container-fluid ">
+  <div class="navbar navbar-fixed-top">
+    <div class="navbar-header">
+      <a href="#" class="navbar-brand">Khaza.com</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Products</a></li>
+      <li><a href="#">Contact Us</a></li>
+    </ul>
+  </div><br><br><br>
+  
+  	
+  <div class="row">
+  	<div class="col-sm-12 center" >
+  		<h2>WELCOME!</h2>
+  		<h4>Who we are</h4>
+  		<p>"OnlineKhaja is a platform developed for cravoing food lovers to get lost in the maza of cuisines of diffrent tastes.</p>
+
+  	</div>
+  </div>
+  <br><br><br><br><br><br>
+<div class="container center">
+	<h2>WHY PEOPLE CHOOSE US</h2>
+	<h4>Clients' Most Popular Choise</h4>
+	<div class="row"  >
+		<div class="col-sm-4" >
+			<h4>INSTANT DELIVERY</h4>
+			<p>We take responsibility for making sure that ordeRs are delivered to you safe and timely</p>
+			<br>
+			<br>
+			<h4>WARM FOOD AT YOUR DOOR</h4>
+			<P>We have smart bags which keep your food fresh and warm for longer period of time</P><br><br>
+			<h4>MORE CHOICES OF RESTAURANTS</h4>
+			<p>We ar working with many restaurants at your location to give you choices of varieties of dishes</p>
+		</div>
+		<div class="col-sm-4">
+			<img src="photo/mobile.jpg" width="41%">
+		</div>
+		<div class="col-sm-4" >
+			<h4>TRACK YOUR ORDER</h4>
+			<p>We have a special festure in our system to track your order and get updates on the status of your order through notification</p><br><br>
+			<h4>FREQUENT FEEDBACK AND REVIEW</h4>
+			<p>We care about our customeRs .Hence, we take your feedback and review them time to time to provide you better delivery service</p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
