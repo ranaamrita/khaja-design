b8e75d632bd97f7341bfa1f76a4cd5d6b032ec2e
ranaamrita <ranaamrita556.com>
2019-01-26 16:59:59 +0545

    first commit

---
+++ navbar/footer.php
@@ -0,0 +1,70 @@
+<footer>
+	<div class="container-fluid footer" style="margin-top: 50px;">
+		<div class="row">
+		  <div class="col-md-2">
+
+		  </div>
+		  <div class="col-md-8" style="color: white;">
+			<h1 class="text-center">WANT COUPONS AND WONDERFUL NEWS ABOUT FOOD?</h1>
+		  </div><br>
+		  <div class="col-md-2">
+			
+		  </div>
+        </div>
+		   <div class="row">
+		  	
+				<div class="col-md-3">
+				<h2><b>KHAJA</b></h2>
+				<p>
+					Famous for the fast food in their own city!!!
+				</p>
+				<span class="glyphicon glyphicon-home">Nepal,Dhangadhi City,Kailai.</span>
+				<span class="glyphicon glyphicon-envelope" >khaja.com</span>
+				<span class="glyphicon glyphicon-phone">+9775800123,97282233883</span>	
+				</div>
+				<div class="col-md-3">
+					<h4 class="text">LINKS</h4>
+					   <li>
+						 <a href="#">My Product</a>
+					   </li>
+		               <li>
+						 <a href="#">My Orders</a>
+					   </li>
+						<li>
+							<a href="#">My Address</a>
+						</li>
+						<li>
+							<a href="#">My Personal Info</a>
+						</li>
+						<li>
+							<a href="#">My Status</a>
+						</li>
+						<li>
+						  <a href="#">Term Of Use</a>
+					    </li>
+
+				</div>
+				<div class="col-md-3">
+					<h4 class="text">CONTACT DETAILS</h4>
+				     <li>
+							<a href="#">Contact Number</a>
+						</li>
+						<li>
+							<a href="#">Email Address</a>
+						</li>
+						<li>
+						  <a href="#"></a>
+					    </li>
+				</div>
+			   <!--  <div class="col-md-3">
+					<h4 class="text">SUBSCRIBE</h4>
+					<p>Integer vitae libero ac risus egestas</p>
+				       <input class="text-center1" type="text" placeholder="Your email address" name="email"aria-labelledby="block-newsletter-label" required>
+					<p>*Don't worry,we wont span</p>
+				</div>
+ -->			</div>
+		</div>
+			
+    </div>
+	
+</footer>
